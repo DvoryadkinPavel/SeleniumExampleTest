f2398a982610986506422b94a44613ecd7529535
Dvoryadkin Pavel <dvoryadkinpavel@mail.ru>
2021-04-06 03:05:42 +0700

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumExampleTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var driver = new ChromeDriver();
+            driver.Url = "http://google.ru";
+            Assert.IsTrue(driver.Url.Contains("google"),"Что-то пошло не так");
+        }
+    }
+}
